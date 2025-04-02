1aa281f8914371dd65812e6fd1ea148f1b64cee2
Cuutu <32869813+Cuutu@users.noreply.github.com>
2025-04-02 16:07:04 -0300

    A ver si anda

--- src/app/admin/cursos/[id]/editar/page.tsx
+++ src/app/admin/cursos/[id]/editar/page.tsx
@@ -1,9 +1,13 @@
 import EditarCursoClient from './EditarCursoClient';
 
+type PageProps = {
+  params: { id: string } & Promise<any>;
+  searchParams?: { [key: string]: string | string[] | undefined };
+};
+
 export default async function EditarCursoPage({
   params,
-}: {
-  params: { id: string }
-}) {
-  return <EditarCursoClient id={params.id} />;
+}: PageProps) {
+  const id = params.id;
+  return <EditarCursoClient id={id} />;
 } 
